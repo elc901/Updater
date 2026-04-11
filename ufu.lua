4094bef46bd78027b06cc56a877704c5e09e24ba
elc901 <kvadiksofficial@gmail.com>
2026-04-11 10:10:01 +0300

    Add files via upload

--- ufu.lua
+++ ufu.lua
@@ -1,28 +1,5 @@
-local repo = "https://github.com/elc901/updater"
-local error = false
-
-local function cmd(c)
-    local h = io.popen(c)
-    if not h then return false end
-    h:close()
-    return true
-end
-
-cmd('rmdir /s /q "updater" 2>nul')
-
-local zip = "updater.zip"
-local url = repo .. "/archive/refs/heads/main.zip"
-if not cmd(string.format('powershell -c "Invoke-WebRequest -Uri \'%s\' -OutFile \'%s\'"', url, zip)) then
-    error = true
-elseif not cmd(string.format('powershell -c "Expand-Archive -Force \'%s\' \'.\'"', zip)) then
-    error = true
-elseif not cmd('move updater-main\\* updater\\ 2>nul') then
-    error = true
-else
-    cmd('rmdir /s /q updater-main 2>nul')
-    cmd('del /q ' .. zip .. ' 2>nul')
-end
-
-local f = io.open("updater_status.txt", "w")
-f:write(error and "True" or "False")
-f:close()
+local repo-link = "https://github.com/elc901/updater/tree/main"
+local function download_files(url, output_path):
+    local cmd
+    if package.config:sub(1, 1) == "\\" then
+        cmd = string.format
